c9041fd501fc5cd7e9b148314b51d8bd2b809e06
namanchauhan1026 <namanchaauhanvv@gmail.com>
2024-07-19 14:57:05 +0530

    setep config

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ dotenv.config({
 connectDB()
 .then( () =>{
     app.listen(process.env.PORT || 8000, () =>{
-        console.log(`Server is running at port : $
-            {process.env.PORT}`);
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
     app.on("error", (error) =>{
         console.log("ERROR: ", error);
